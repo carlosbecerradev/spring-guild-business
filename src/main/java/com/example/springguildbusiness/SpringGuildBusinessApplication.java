5d6615a1acf859dccf7b13d2100d6915df006f05
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-09-09 11:50:38 -0500

    Initial commit

---
+++ src/main/java/com/example/springguildbusiness/SpringGuildBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.example.springguildbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuildBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuildBusinessApplication.class, args);
+	}
+
+}
