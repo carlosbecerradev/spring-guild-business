7838f72ae8aaeb546e06ae6f56909a3529a493bf
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-09-10 18:12:17 -0500

    Added send mail method. Added thymeleaf to return the message in the HTML format

---
+++ src/main/java/com/example/springguildbusiness/service/MailService.java
@@ -0,0 +1,37 @@
+package com.example.springguildbusiness.service;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import com.example.springguildbusiness.model.NotificationEmail;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+
+	private final JavaMailSender mailSender;
+	private final MailContentBuilder mailContentBuilder;
+	
+	void sendMail(NotificationEmail notificationEmail) {
+		MimeMessagePreparator messagePreparator = mimeMessage -> {
+			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+			messageHelper.setFrom("spring.email@email.com");
+			messageHelper.setTo(notificationEmail.getRecipient());
+			messageHelper.setSubject(notificationEmail.getSubject());
+			messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+		};
+		try {
+			mailSender.send(messagePreparator);
+			log.info("Activation email sent!!!");
+		} catch (MailException e) {
+			// TODO: handle exception
+		}
+	}
+}
