282d88630f5e870334c1062fcd8d46cae5042333
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-09-10 18:23:13 -0500

    I forgot

--- src/main/java/com/example/springguildbusiness/service/AuthService.java
+++ src/main/java/com/example/springguildbusiness/service/AuthService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.springguildbusiness.dto.RegisterRequest;
 import com.example.springguildbusiness.model.User;
@@ -22,6 +23,7 @@ public class AuthService {
 	private final UserRepository userRepository;
 	private final VerificationTokenRepository verificationTokenRepository;
 	
+	@Transactional
 	public void signup(RegisterRequest registerRequest) {
 		User user = new User();
 		user.setUsername(registerRequest.getUsername());
