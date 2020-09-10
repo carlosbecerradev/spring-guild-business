4012075a68b08cdfa3c397207f9c164c85c88d59
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-09-09 19:00:05 -0500

    Added generate token method to activate user

---
+++ src/main/java/com/example/springguildbusiness/repository/VerificationTokenRepository.java
@@ -0,0 +1,11 @@
+package com.example.springguildbusiness.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.springguildbusiness.model.VerificationToken;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+
+}
--- src/main/java/com/example/springguildbusiness/service/AuthService.java
+++ src/main/java/com/example/springguildbusiness/service/AuthService.java
@@ -1,13 +1,16 @@
 package com.example.springguildbusiness.service;
 
 import java.time.Instant;
+import java.util.UUID;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.springguildbusiness.dto.RegisterRequest;
 import com.example.springguildbusiness.model.User;
+import com.example.springguildbusiness.model.VerificationToken;
 import com.example.springguildbusiness.repository.UserRepository;
+import com.example.springguildbusiness.repository.VerificationTokenRepository;
 
 import lombok.AllArgsConstructor;
 
@@ -17,6 +20,7 @@ public class AuthService {
 
 	private final PasswordEncoder passwordEncoder;
 	private final UserRepository userRepository;
+	private final VerificationTokenRepository verificationTokenRepository;
 	
 	public void signup(RegisterRequest registerRequest) {
 		User user = new User();
@@ -27,5 +31,17 @@ public class AuthService {
 		user.setEnabled(false);
 		
 		userRepository.save(user);
+		
+		generateVerificationToken(user);
+	}
+
+	private String generateVerificationToken(User user) {
+		String token = UUID.randomUUID().toString();
+		VerificationToken verificationToken = new VerificationToken();
+		verificationToken.setToken(token);
+		verificationToken.setUser(user);
+		
+		verificationTokenRepository.save(verificationToken);
+		return token;
 	}
 }
